772527bd58ea8323344b0286f8d0332da15eee3e
Code With Dhruv <codewithdhruv22@gmail.com>
2023-06-24 13:22:54 +0530

    Creating Server

--- server/index.js
+++ server/index.js
@@ -1,1 +1,12 @@
 console.log("Kaise hain aap log ?");
+
+const express = require('express'); // import '....material.dart'
+
+const app  = express(); // dataModel = DataModel();
+const PORT = 3000;
+// 0.0.0.0 -- Anywhere
+app.listen(PORT, "0.0.0.0", function(){
+    console.log("NODEJS SERVER STARTED...");
+})
+
+
